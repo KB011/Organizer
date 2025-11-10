58c3fad4e13941d5f2ca0ad068009f2748dc1a0e
Kaustav <kaustav.std@gmail.com>
2025-11-10 10:56:03 +0530

    configure global logger

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,2 +1,3 @@
-export { default as gracefulShutdown } from './graceful-shutdown';
-export * from './prisma-singleton';
+export { default as gracefulShutdown } from '@server/config/graceful-shutdown';
+export * from '@server/config/prisma-singleton';
+export { default as logger } from '@server/config/logger';
--- src/server/middlewares/error-handler.ts
+++ src/server/middlewares/error-handler.ts
@@ -1,12 +1,18 @@
 import { AppError } from '@server/utils';
 import { NextFunction, Request, Response } from 'express';
+import { logger } from '@server/config';
 
 const globalErrorHandler = (
   err: Error | AppError,
-  _req: Request,
+  req: Request,
   res: Response,
   _next: NextFunction
 ) => {
+  logger.error(`Error on ${req.method} ${req.url} - ${err.message}`, {
+    stack: err.stack,
+    statusCode: (err as AppError).statusCode || 500,
+  });
+
   if (err instanceof AppError) {
     res.status(err.statusCode).json({
       success: false,
