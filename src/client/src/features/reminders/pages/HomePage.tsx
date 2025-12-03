50da4da0c52b907b206f5a01d94749c844a28ba7
Kaustav <kaustav.std@gmail.com>
2025-12-04 02:02:31 +0530

    demo component creation

--- src/client/src/features/reminders/pages/HomePage.tsx
+++ src/client/src/features/reminders/pages/HomePage.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@client/components/ui/button';
+
 export default function HomePage() {
-  return <h1 className="text-3x1 font-bold">Welcome to Organizer</h1>;
+  return (
+    <>
+      <h1 className="text-3x1 font-bold">Welcome to Organizer</h1>
+      <div className="p-6">
+        <Button>Click Me!</Button>
+      </div>
+    </>
+  );
 }
